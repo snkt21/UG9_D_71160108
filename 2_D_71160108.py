9faa799434d19a4fd915c35f1a5a1acb218c7e86
Surya NKT <surya.nathaniel@ti.ukdw.ac.id>
2021-11-11 19:02:32 +0700

    71160108_Tambah soal 2

---
+++ 2_D_71160108.py
@@ -0,0 +1,11 @@
+#Surya Nathaniel Kattu
+#Nim: 71160108
+#Soal No 2
+
+usa1 = int(input("Uang saku anak pertama : "))
+usa2 = int(input("Uang saku anak kedua : "))
+usa3 = int(input("Uang saku anak ketiga : "))
+
+rata2 = 1/3*(usa1+usa2+usa3) #rumus rata - rata uang saku: jumlah uang anak pertama - ketiga / 3 (jumlah anak)
+
+print ("Rata-rata dari ", usa1, "+", usa2, "+", usa3, "adalah", rata2)
