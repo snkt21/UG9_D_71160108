30193fbe83ad277472f89d76dde0140c9bc03643
Surya NKT <surya.nathaniel@ti.ukdw.ac.id>
2021-11-11 19:02:51 +0700

    71160108_Tambah soal 3

---
+++ 3_D_71160108.py
@@ -0,0 +1,12 @@
+#Surya Nathaniel Kattu
+#Nim: 71160108
+#Soal No 3
+
+mdb1 = str(input("Masukkan daftar belanja 1 : ")) #menggunakan type data string
+mdb2 = str(input("Masukkan daftar belanja 2 : ")) #menggunakan type data string
+
+tdb1 = str(input("Tambahkan data ke daftar belanja 1 : ")) #menggunakan type data string
+tdb2 = str(input("Tambahkan data ke daftar belanja 2 : ")) #menggunakan type data string
+
+print ("Daftar belanja 1 adalah ", mdb1, tdb1)
+print ("Daftar belanja 2 adalah ", mdb2, tdb2)
