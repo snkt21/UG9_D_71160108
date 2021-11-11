ac0c01100c1a4d232e12b61b464b0f810a41a086
Surya NKT <surya.nathaniel@ti.ukdw.ac.id>
2021-11-11 19:02:14 +0700

    71160108_Tambah soal 1

---
+++ 1_D_71160108.py
@@ -0,0 +1,10 @@
+#Surya Nathaniel Kattu
+#Nim: 71160108
+#Soal No 1
+
+dgl1 = int(input("Masukkan d1 : "))
+dgl2 = int(input("Masukkan d1 : "))
+
+luas = 0.5*(dgl1*dgl2) #rumus luas layangan : 1/2 (diagonal 1 * diagonal 2)
+
+print ("Luas layangan: ", luas, "cm")
